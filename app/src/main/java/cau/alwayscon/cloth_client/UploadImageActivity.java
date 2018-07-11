7269406351dee566792ead234122524913e691b5
shaaaaanxi <2883267@naver.com>
2018-07-11 17:58:40 +0900

    !

--- app/src/main/java/cau/alwayscon/cloth_client/UploadImageActivity.java
+++ app/src/main/java/cau/alwayscon/cloth_client/UploadImageActivity.java
@@ -1,33 +1,24 @@
 package cau.alwayscon.cloth_client;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class UploadImageActivity extends AppCompatActivity implements View.OnClickListener{
-    ImageView uploadedImage;
-    ImageButton bUploadImage;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload_image);
+public class UploadImageActivity {
 
-        uploadedImage =(ImageView) findViewById(R.id.uploadedImage);
-
-        bUploadImage = (ImageButton) findViewById(R.id.bUploadImage);
-
-        uploadedImage.setOnClickListener(this);
-        bUploadImage.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
 }

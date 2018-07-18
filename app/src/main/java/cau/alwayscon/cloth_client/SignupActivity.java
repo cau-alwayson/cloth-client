1f1e3e5da51e9b9f6641c170f2a01f943c544207
yg0585@naver.com <mh.135g2>
2018-07-18 16:58:04 +0900

    영광짱짱맨이 뷰 겹치는걸 수정하였음.

--- app/src/main/java/cau/alwayscon/cloth_client/SignupActivity.java
+++ app/src/main/java/cau/alwayscon/cloth_client/SignupActivity.java
@@ -3,15 +3,24 @@ package cau.alwayscon.cloth_client;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
+    ImageView cancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        cancel = (ImageView)findViewById(R.id.cancel);
+
+        cancel.bringToFront();
+
+
     }
 
     public void onBackButton2Clicked(View v) {

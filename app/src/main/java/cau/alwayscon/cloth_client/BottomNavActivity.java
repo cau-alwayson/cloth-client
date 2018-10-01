fe5fb28f018209a8ae86ea0bd831d4325e1e80a6
Myeongwon Choi <auspic7@gmail.com>
2018-10-01 15:36:50 +0900

    Bottom Navigation Activity implementation.

---
+++ app/src/main/java/cau/alwayscon/cloth_client/BottomNavActivity.java
@@ -0,0 +1,48 @@
+package cau.alwayscon.cloth_client;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class BottomNavActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.friend_list:
+                    mTextMessage.setText(R.string.friend_list);
+                    return true;
+                case R.id.best_coordi:
+                    mTextMessage.setText(R.string.best_coordi);
+                    return true;
+                case R.id.my_page:
+                    mTextMessage.setText(R.string.my_page);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/cau/alwayscon/cloth_client/MainActivity.java
+++ app/src/main/java/cau/alwayscon/cloth_client/MainActivity.java
@@ -5,13 +5,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bottomButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomButton = findViewById(R.id.button11);
+        bottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),BottomNavActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void LOGIN(View v){

ade88eda8f65c3ef9693f6d87214e0c1b0056aee
dlguswn3659 <39727494+dlguswn3659@users.noreply.github.com>
2018-10-16 00:51:38 +0900

    강좌보고 아저씨가 하라는대로 해서 fragment만들었는데 ㅜㅜ 빌드 오류는 안뜨는데 결정적으로 네비게이션 바가 안보인다... apk설치해서 실행했더니 튕긴다...8ㅅ8 내가 보기엔 내가 영상이랑 완전 비슷하게 이름도 만들고 구조도 바꾸고 해서
    결정적으로
    Toolbar 부분을 모조리... 지워버렸는데...
    
    아조씨...
    
    네비게이션바가 다시 보일 수 있게 심폐소생해주실 천사 구합니다ㅠㅠㅠㅠㅠ
    
    참고로 menu폴더 안에있던 기존의 bottom_Nevigation_menu.xml이었나 그거 이름을 nev_items로 바꾸고 거기 안에 있던 id랑 title도 이름 수정했어요!
    
    25.0.0이라고 돼있는데는 27.1.1로 바꿨구여

--- app/src/main/java/cau/alwayscon/cloth_client/BottomNavActivity.java
+++ app/src/main/java/cau/alwayscon/cloth_client/BottomNavActivity.java
@@ -1,48 +1,77 @@
 package cau.alwayscon.cloth_client;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class BottomNavActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FrameLayout mMainFrame;
+    private BottomNavigationView mMainNav;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.friend_list:
-                    mTextMessage.setText(R.string.friend_list);
-                    return true;
-                case R.id.best_coordi:
-                    mTextMessage.setText(R.string.best_coordi);
-                    return true;
-                case R.id.my_page:
-                    mTextMessage.setText(R.string.my_page);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private FriendlistFragment friendlistFragment;
+    private BestcodiFragment bestcodiFragment;
+    private MypageFragment mypageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_nav);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        friendlistFragment = new FriendlistFragment();
+        bestcodiFragment = new BestcodiFragment();
+        mypageFragment = new MypageFragment();
+
+        setFragment(friendlistFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.nav_friendlist :
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(friendlistFragment);
+                        return true;
+
+                    case R.id.nav_bestcodi :
+                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(bestcodiFragment);
+                        return true;
+
+                    case R.id.nav_mypage :
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(mypageFragment);
+                        return true;
+
+                        default:
+                            return false;
+                }
+            }
+        });
+
+
+
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
     }
 
 }
--- app/src/main/java/cau/alwayscon/cloth_client/LoginActivity.java
+++ app/src/main/java/cau/alwayscon/cloth_client/LoginActivity.java
@@ -1,21 +1,95 @@
 package cau.alwayscon.cloth_client;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import cau.alwayscon.cloth_client.LoginRequest;
+import cau.alwayscon.cloth_client.MainActivity;
+import cau.alwayscon.cloth_client.R;
+import cau.alwayscon.cloth_client.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button btn_login;
+    private TextView link_regist;
+    private  static String URL_LOGIN="http://sjly3k.cafe24.com/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+        setContentView(R.layout.login_screen);
 
+        final EditText idText = (EditText)findViewById(R.id.layoutemail);
+        final EditText passwordText = (EditText)findViewById(R.id.layoutpassword);
+        final Button loginButton = (Button) findViewById(R.id.btn_login);
+        final TextView registerButton = (TextView)findViewById(R.id.btn_register);
 
-    public void onBackButton4Clicked(View v) {
-        Toast.makeText(getApplicationContext(), "돌아가기 버튼을 눌렀어요.", Toast.LENGTH_LONG).show();
-        finish();
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String userID = idText.getText().toString();
+                final String userPassword = passwordText.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success)
+                            {
+                                String userID = jsonResponse.getString("userID");
+                                String userPassword = jsonResponse.getString("userPassword");
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("userID", userID);
+                                intent.putExtra("userPassword", userPassword);
+                                LoginActivity.this.startActivity(intent);
+                            }
+                            else
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("로그인에 실패하였습니다.")
+                                        .setNegativeButton("다시 시도", null)
+                                        .create()
+                                        .show();
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+
+        });
+
     }
 }

a92aa01c1a63eca7e2145bc4f82b978fe2f66603
auspic7 <auspic7@gmail.com>
2018-10-15 18:06:57 +0900

    Login function with web integration implemented

--- app/src/main/java/cau/alwayscon/cloth_client/SignupActivity.java
+++ app/src/main/java/cau/alwayscon/cloth_client/SignupActivity.java
@@ -1,30 +1,113 @@
 package cau.alwayscon.cloth_client;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class SignupActivity extends AppCompatActivity {
 
     ImageView cancel;
+    EditText username, password, nickname;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         cancel = (ImageView)findViewById(R.id.cancel);
+        username = (EditText)findViewById(R.id.button8);
+        password = (EditText)findViewById(R.id.button7);
+        nickname = (EditText)findViewById(R.id.nick);
+        registerButton = (Button)findViewById(R.id.button5);
 
         cancel.bringToFront();
 
-
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new GetUrlContentTask().execute("http://150.95.180.235/register.php?username="+username.getText()+"&password="+password.getText()+"&nickname="+nickname.getText());
+            }
+        });
     }
 
     public void onBackButton2Clicked(View v) {
         Toast.makeText(getApplicationContext(), "돌아가기 버튼을 눌렀어요.", Toast.LENGTH_LONG).show();
         finish();
+    }
+
+    private class GetUrlContentTask extends AsyncTask<String, Integer, String> {
+        protected String doInBackground(String... urls) {
+            URL url = null;
+            try {
+                url = new URL(urls[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.setRequestMethod("GET");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            try {
+                connection.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            BufferedReader rd = null;
+            try {
+                rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String content = "", line;
+            try {
+                while ((line = rd.readLine()) != null) {
+                    content += line + "\n";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return content;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+        }
+
+        protected void onPostExecute(String result) {
+            // this is executed on the main thread after the process is over
+            // update your UI here
+            System.out.println(result);
+            result = result.replace("\n", "");
+            if (result.equals("0")) {
+                Toast.makeText(getApplicationContext(), "가입에 실패했어요.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "가입에 성공했어요. : " + result, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
